2dc7b918ff99d80a7c48059c21f79f3857da427a
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2021-03-16 23:53:54 -0600

    added intro page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="intro d-flex justify-content-center align-items-center">
+      <div>
+        <h1>Bruce Nguyen</h1>
+        <p>Software Engineer</p>
+        <button>Contact Me</button>
+      </div>
+    </div>
+    )
 }
