2a2150ad87b9633714887464a2621cdc063d706b
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2021-03-17 17:09:48 -0600

    added avatar

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div className="intro d-flex justify-content-center align-items-center">
       <div>
+        <Img
+          className="avatar ml-auto mr-auto"
+          fixed={data.file.childImageSharp.fixed}
+          alt="Me"
+        />
         <h1>Bruce Nguyen</h1>
         <p>Software Engineer</p>
         <button>Contact Me</button>
@@ -11,3 +18,15 @@ export default function Home() {
     </div>
     )
 }
+
+export const query = graphql`
+  query MyQuery {
+    file(relativePath: { eq: "avatar1.png" }) {
+      childImageSharp {
+        fixed(width: 115) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
