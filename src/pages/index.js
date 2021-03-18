8bc7b1e38f2024c9f96f92dffa10febabff13d3f
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2021-03-18 01:14:25 -0600

    added about-me section

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,17 @@ export default function Home({ data }) {
         </div>
       </div>
       <IntroParallax />
+      <div className="about-me d-flex justify-content-center w-75 ml-auto mr-auto">
+        <Img
+          className="avatar d-flex mt-auto mb-auto"
+          fixed={data.file.childImageSharp.fixed}
+          alt="Me"
+        />
+        <div className="container mt-auto mb-auto">
+          <h1>My Story</h1>
+          <p>I am Bruce Nguyen, software developer from Utah, United States. I have professional experience in data engineering using Python, Redis, Docker, AWS to ingest data and transform into readable format. On the side, I like to create web applications using React and Django. I'm also self motivated to learn about new technologies to help create efficient solutions that could be applied programmatically.</p>
+        </div>
+      </div>
     </section>
   )
 }
