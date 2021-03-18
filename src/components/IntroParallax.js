c94c0b0494e99b5b38e55d4f76fe046b8ff9deb1
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2021-03-18 00:58:37 -0600

    added Shape2

--- src/components/IntroParallax.js
+++ src/components/IntroParallax.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import Parallax from "parallax-js";
 import Shape1 from "./Shapes/Shape1";
+import Shape2 from "./Shapes/Shape2";
 
 export default function IntroParallax() {
 
@@ -14,6 +15,7 @@ export default function IntroParallax() {
   return (
     <div className="parallax" data-relative-input="true">
       <Shape1 />
+      <Shape2 />
     </div>
   )
 }
---
+++ src/components/Shapes/Shape2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Shape2() {
+  return (
+    <svg
+      className="layer shape2"
+      data-depth="0.4"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 72 72"
+    >
+      <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" fill="#00B4E4" />
+    </svg>
+  );
+}
