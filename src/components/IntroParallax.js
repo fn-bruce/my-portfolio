ceb139f6c6f182fafea656e881f6f8d7805456d7
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2021-03-19 02:10:08 -0600

    added more shapes

--- src/components/IntroParallax.js
+++ src/components/IntroParallax.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react"
 import Parallax from "parallax-js";
 import Shape1 from "./Shapes/Shape1";
 import Shape2 from "./Shapes/Shape2";
+import Shape3 from "./Shapes/Shape3";
+import Shape4 from "./Shapes/Shape4";
 
 export default function IntroParallax() {
 
@@ -16,6 +18,8 @@ export default function IntroParallax() {
     <div className="parallax" data-relative-input="true">
       <Shape1 />
       <Shape2 />
+      <Shape3 />
+      <Shape4 />
     </div>
   )
 }
---
+++ src/components/Shapes/Shape3.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Shape3() {
+  return (
+    <svg
+      className="layer shape3"
+      data-depth="0.3"
+      width="6"
+      height="6"
+      viewBox="0 0 6 6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M5.42016 0H0V5.42019H5.42016V0Z" fill="white" />
+    </svg>
+  );
+}
